5ff75510025262252c55db0b68abe15589b806bd
mahdizahedii <59919435+mahdizahedii@users.noreply.github.com>
2021-10-30 20:17:03 +0330

    Update cantatc.php

--- call/cantatc.php
+++ call/cantatc.php
@@ -5,7 +5,7 @@ var_dump( $_GET );
 var_dump( $_REQUEST );
 */
 // 1.connect to DB - mysql - mysqli - PDO
-$dbc = new mysqli ('localhost', 'root', '', 'message'); // mysqli_connect
+$dbc = new mysqli ('localhost', 'root', '', 'call'); // mysqli_connect
 // 2. create insert query
 $sql = "INSERT INTO message(title, body)
 		VALUES('my title', 'متن پیام')";
